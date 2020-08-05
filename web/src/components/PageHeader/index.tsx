cc1454fd1775c9754dcad01bb9b06aaa477a3f09
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2020-08-04 21:22:22 -0300

    Add TeacherList

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 import logoIcon from '../../assets/images/logo.svg'; 
 import backIcon from '../../assets/images/icons/back.svg'; 
 
-function PageHeader() {
+interface PageHeaderProps {
+  title: string;
+}
+
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
   return(
     <header className="page-header">
       <div className="top-bar-container">
@@ -15,7 +19,8 @@ function PageHeader() {
       </div>
 
       <div className="header-content">
-        <strong>Estes são os proffys disponíveis.</strong>
+        <strong>{props.title}</strong>
+        {props.children}
       </div>
     </header> 
   );
