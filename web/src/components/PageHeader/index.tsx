e6c776b99ae2c4c162dbd5a1b27c3b707c32b44d
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2020-08-04 21:02:27 -0300

    Add styles PageHeader

---
+++ web/src/components/PageHeader/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logoIcon from '../../assets/images/logo.svg'; 
+import backIcon from '../../assets/images/icons/back.svg'; 
+
+function PageHeader() {
+  return(
+    <header className="page-header">
+      <div className="top-bar-container">
+        <Link to="/">
+          <img src={backIcon} alt="Voltar"/>
+        </Link>
+          <img src={logoIcon} alt="Proffy"/>
+      </div>
+
+      <div className="header-content">
+        <strong>Estes são os proffys disponíveis.</strong>
+      </div>
+    </header> 
+  );
+}
+
+export default PageHeader;
