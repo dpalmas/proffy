d928ad6fcf62045e602229fdf6983b8247360204
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2020-08-06 12:50:32 -0300

    Add controllers

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,6 +4,7 @@ import ClassesController from './controllers/ClassesController';
 const routes = express.Router();
 const classesControllers = new ClassesController();
 
+routes.get('/classes', classesControllers.index); 
 routes.post('/classes', classesControllers.create); 
 
 export default routes;
