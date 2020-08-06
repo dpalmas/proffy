1bcfd9af9828b8055dbe168c7456d2dba0305f45
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2020-08-06 12:08:53 -0300

    Create database

---
+++ server/src/routes.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import ClassesController from './controllers/ClassesController';
+
+const routes = express.Router();
+const classesControllers = new ClassesController();
+
+routes.post('/classes', classesControllers.create); 
+
+export default routes;
