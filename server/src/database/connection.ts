8ce4607005a520ad42b1e9f09a86b27f9b6ff94a
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2020-08-05 21:09:33 -0300

    Add back-end

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -5,5 +5,8 @@ const db = knex({
   client: 'sqlite3',
   connection: {
     filename: path.resolve(__dirname, 'database.sqlite')
-  }
-})
+  },
+  useNullAsDefault: true,
+});
+
+export default db;
