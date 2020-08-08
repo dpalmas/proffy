0c1afeef52f07832e78c36e34b820e0a453fb621
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2020-08-07 22:08:26 -0300

    Update front-end

---
+++ web/src/components/Input/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface InputProps {
+  name: string;
+  label: string;
+}
+
+const Input: React.FC<InputProps> = ({ label, name }) => {
+  return (
+    <div className="input-block">
+      <label htmlFor={name}>{label}</label>
+      <input type="text" id={name}/>
+  </div>
+  );
+}
+
+export default Input;
